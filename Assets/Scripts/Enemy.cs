badbf521ecca83e997f7bd9e4457238d194dbd48
hpcergar <hpcergar@gmail.com>
2022-02-16 22:33:34 +0100

    Add score board. Add enemy hit points

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject hitVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int healthPoints = 10;
     [SerializeField] int scorePerHit = 10;
+    private int damagePerHit = 10;
 
     ScoreBoard scoreBoard;
 
@@ -17,8 +20,37 @@ public class Enemy : MonoBehaviour
 
     private void OnParticleCollision(GameObject other) 
     {
-        ProcessHit();
-        KillEnemy();
+        this.ProcessHit();
+        if(this.healthPoints > 0) {
+            HitEnemy(other);
+        } else {
+            KillEnemy();
+        }
+    }
+
+    private void ProcessHit()
+    {
+        this.healthPoints -= damagePerHit;
+        this.scoreBoard.IncreaseScore(this.scorePerHit);
+    }
+
+    private void HitEnemy(GameObject other)
+    {
+        ParticleSystem particleSystem = other.GetComponent<ParticleSystem>();
+        List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>();
+        particleSystem.GetCollisionEvents(this.gameObject, collisionEvents);
+
+        foreach(var collisionEvent in collisionEvents)
+        {
+            this.displayHitVfx(collisionEvent.intersection);
+        }
+    }
+
+    private void displayHitVfx(Vector3 position)
+    {
+        GameObject vfx = Instantiate(hitVFX, position, Quaternion.identity);
+        vfx.transform.parent = this.parent;
+        vfx.AddComponent<SelfDestruct>();
     }
 
     private void KillEnemy()
@@ -27,10 +59,5 @@ public class Enemy : MonoBehaviour
         vfx.transform.parent = this.parent;
         vfx.AddComponent<SelfDestruct>();
         Destroy(this.gameObject);    
-    }
-
-    private void ProcessHit()
-    {
-        this.scoreBoard.IncreaseScore(this.scorePerHit);
     }
 }
--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,14 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
     private int score = 0;
+    private TMP_Text text;
+
+    private void Start() 
+    {
+        this.text = GetComponent<TMP_Text>();
+        text.SetText("Start!");
+    }
     
     public void IncreaseScore(int quantity)
     {
         this.score += quantity;
-        Debug.Log(this.score);
+        refreshUI();
+    }
+
+    private void refreshUI()
+    {
+        text.SetText(score.ToString());
     }
 }
