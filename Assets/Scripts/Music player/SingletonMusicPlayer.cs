33bc3736c4481f64fd3fe98a6ed013979541b420
hpcergar <hpcergar@gmail.com>
2022-06-20 22:57:55 +0200

    Use OGG audio. Implement audio loops with offsets

--- Assets/Scripts/Music player/SingletonMusicPlayer.cs
+++ Assets/Scripts/Music player/SingletonMusicPlayer.cs
@@ -1,10 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class SingletonMusicPlayer : MonoBehaviour
 {
     [SerializeField] float startOffset = 0f;
+    [SerializeField] bool loop = false;
+    [SerializeField] float loopOffset = 0f;
+
+    // Cache
+    private AudioSource audioSource;
 
     private void Awake()
     {
@@ -21,6 +25,19 @@ public class SingletonMusicPlayer : MonoBehaviour
 
     private void Start()
     {
-        this.GetComponent<AudioSource>().time += this.startOffset;
+        this.audioSource = this.GetComponent<AudioSource>();
+        this.audioSource.time = this.startOffset;
+    }
+
+    private void Update() 
+    {
+        if(this.audioSource.isPlaying) {
+            return;
+        }
+
+        if(this.loop) {
+            this.audioSource.time = this.loopOffset;
+            this.audioSource.Play();
+        }
     }
 }
