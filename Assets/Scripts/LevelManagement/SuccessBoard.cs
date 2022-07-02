b3ff9eadff5681afa0867fe9bae432d83f049858
hpcergar <hpcergar@gmail.com>
2022-07-02 22:33:58 +0200

    Fix remaining score and death count

--- Assets/Scripts/LevelManagement/SuccessBoard.cs
+++ Assets/Scripts/LevelManagement/SuccessBoard.cs
@@ -10,9 +10,16 @@ public class SuccessBoard : MonoBehaviour
         this.text = GetComponent<TMP_Text>();
         int playerScore  =  PlayerPrefs.GetInt("score");
         int playerDeaths  =  PlayerPrefs.GetInt("deaths");
+        this.ResetUserStats();
         text.SetText("Final score: " + playerScore.ToString() 
         + "\n" + "Deaths: " + playerDeaths.ToString()
         + "\n" + "Congratulations!"
         );
+    }
+
+    private void ResetUserStats()
+    {
+        PlayerPrefs.SetInt("score", 0);
+        PlayerPrefs.SetInt("deaths", 0);
     }
 }
